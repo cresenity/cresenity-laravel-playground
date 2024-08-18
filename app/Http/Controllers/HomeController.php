e62d40efb9daabc99c65a813df3ef12612d24fc6
Hery <hery@ittron.co.id>
2024-08-19 02:02:52 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App;
+use c;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -12,8 +14,12 @@ class HomeController extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function index()
     {
+        c::manager()->theme()->setTheme('cl-web');
         $app = \c::app();
         $app->addDiv()->add("Hello");
+        $app->addDiv()->addA()->setHref(c::url('https://google.com'))->add('Google');
+        $selectSearch = $app->addSelectSearchControl();
+
         return $app;
     }
 }
